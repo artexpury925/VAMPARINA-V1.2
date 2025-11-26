fc63d4762e9420e8c977a317fb6936d82fd582ce
artexpury925 <artexpury@gmail.com>
2025-11-26 12:08:38 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,16 +1,16 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'VAMPARINA V1',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "VAMPARINA V1",
+  botOwner: 'Arnold', // Your name
+  ownerNumber: '254703110780', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
   storeWriteInterval: 10000,
   description: "This is a bot for managing group commands and automating tasks.",
   version: "3.0.5",
-  updateZipUrl: "https://github.com/mruniquehacker/Knightbot-MD/archive/refs/heads/main.zip",
+  updateZipUrl: "https://github.com/artexpury925/VAMPARINA-V1.2/archive/refs/heads/main.zip",
 };
 
 module.exports = settings;
